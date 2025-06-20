26f51d66ff198e52204134efd50c3314b150cba0
Mario ROBERT <mariolourobert@gmail.com>
2025-06-20 22:32:35 +0200

    add room to :database

--- data/database/build.gradle.kts
+++ data/database/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -31,5 +32,8 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
     testImplementation(libs.junit)
 }
