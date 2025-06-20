c27463b45c8133d6d2bad023a6c1faf52f91f197
Mario ROBERT <mariolourobert@gmail.com>
2025-06-20 23:51:21 +0200

    Insert PlaylistTrackCrossRef

--- data/database/src/main/java/com/deezer/exoapplication/database/daos/PlaylistWithTracksDao.kt
+++ data/database/src/main/java/com/deezer/exoapplication/database/daos/PlaylistWithTracksDao.kt
@@ -1,13 +1,18 @@
 package com.deezer.exoapplication.database.daos
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Transaction
+import com.deezer.exoapplication.database.entities.PlaylistTrackCrossRef
 import com.deezer.exoapplication.database.queries.PlaylistWithTracksQueryResult
 
 @Dao
 interface PlaylistWithTracksDao {
     @Transaction
     @Query("SELECT * FROM Playlist WHERE playlistId = :playlistId")
-    fun getPlaylistsWithSongs(playlistId: Int): List<PlaylistWithTracksQueryResult>
+    suspend fun getPlaylistWithTracks(playlistId: Int): PlaylistWithTracksQueryResult?
+
+    @Insert
+    suspend fun insert(playlistTrackCrossRef: PlaylistTrackCrossRef)
 }
